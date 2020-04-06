87e3736579745d143c8cbc5d79320567a9dbf2bf
Folafunmi <folafunmimustapha@gmail.com>
2020-04-06 20:27:31 +0100

    Base design done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 console.clear();
 const elApp = document.querySelector('#app');
 
+function activate(state) {
+    elApp.dataset.state = state;
+
+    document.querySelectorAll(`[data-active]`).forEach(el => delete el.dataset.active);
+    document.querySelectorAll(`[data-view="${state}"]`).forEach(el => el.dataset.active = true);
+
+}
+
+activate('details');
